f1b0c986f22bea6f3244ea21afec8fab0be30768
Derlie Monne <derlie.monne@gmail.com>
2023-10-14 09:30:35 +1000

    feat(facts generation) - clicking on text generates random fact

--- script.js
+++ script.js
@@ -1,3 +1,25 @@
 function onClick() {
-    console.log(4)
+    fact = document.getElementById("fact");
+    fact.innerHTML = randomChoice(facts)
+    
 }
+
+function randomChoice(arr) {
+    return arr[Math.floor(arr.length * Math.random())];
+}
+
+facts = []
+
+async function init() {
+    text = await fetch("facts.txt")
+        .then((request) => request.text())
+    facts = text.split("\n").map(function(fact) {
+        return fact.trim()
+    })
+}  
+
+window.onload = init
+
+
+
+
